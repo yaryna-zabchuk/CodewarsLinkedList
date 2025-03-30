82476294cdc88442935bff10feb499ebba17f6be
Yaryna Zabchuk <yarunazabchuk@gmail.com>
2025-03-30 21:01:24 +0300

    add solution of the problem:Linked Lists - Push & BuildOneTwoThree, and small fixes

--- Convert_a_linked_list_to_a_string/solution.py
+++ Convert_a_linked_list_to_a_string/solution.py
@@ -5,7 +5,9 @@ def stringify(node):
     '''Convert a linked list to a string.'''
     res = ''
     cur = node
+
     while cur:
         res += str(cur.data) + ' -> '
         cur = cur.next
+
     return res + 'None'
---
+++ Linked_Lists_-_Push_&_BuildOneTwoThree/solution.py
@@ -0,0 +1,14 @@
+'''Solution for Codewars problem: Linked Lists - Push & Build One Two Three'''
+from preloaded import Node
+
+def push(head, data):
+    '''Push a new node with data to the front of the linked list.'''
+    new_node = Node(data, head)
+    return new_node
+
+def build_one_two_three():
+    '''Build a linked list with the values 1, 2, and 3.'''
+    head = Node(3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
